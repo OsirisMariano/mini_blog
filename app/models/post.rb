cfde0c76b2d39fc58c3b7cecde945983040735dc
Osiris Mariano <oslimacontato@gmail.com>
2023-06-07 19:55:17 -0300

    Valida title, text em post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,10 @@
 class Post < ApplicationRecord
+  validates :title, :description presence: true
+  validates :title, length: {minimum: 50}
+  validates :description, length: {maximum: 240}
+
+  before_save do
+    self.title = self.title + " Osiris"
+  end
+
 end
